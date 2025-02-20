caa7c6e873670fa95f920c7ca89a905ff4fe54b3
Xtypie <xtypie2505@gmail.com>
2025-02-20 23:15:00 +0300

    Add: code lab 3

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,1 +1,49 @@
 #include<iostream>
+#include<cmath>
+using namespace std;
+
+
+struct Para {
+    private:
+        int a, b, angle, height, per;
+        float area;
+        
+    public:
+        void NumsOfPara()
+        {
+            cout << "enter number a of para" << endl;
+            cin >> a;
+            cout << "enter number b of para" << endl;
+            cin >> b;
+            cout << "enter angle between a and b (if u don`t have enter 0)" << endl;
+            cin >> angle;
+            cout << "enter height of para (if u don`t have enter 0)" << endl;
+            cin >> angle;
+        }
+
+        void Area()
+        {
+            if (height != 0) {
+                area = a * height;
+                cout << "area is " << area << endl;
+            }
+            else if (angle != 0)
+                {
+                    float sinangle = sin(angle);
+                    area = (a * b * sinangle) / 2;
+
+                    cout << "area is " << area << endl;
+                }
+            else 
+                cout << "No data" << endl;
+        }
+        
+        void Per() {
+            cout << "per is " << (a + b) * 2 << endl;
+        }
+};
+
+int main()
+{
+    
+}
