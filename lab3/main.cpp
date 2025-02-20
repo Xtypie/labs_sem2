b769474c45028566d80404d5c4be4dc4e07abbe0
Xtypie <xtypie2505@gmail.com>
2025-02-21 00:58:42 +0300

    done: lab3

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -3,47 +3,38 @@
 using namespace std;
 
 
-struct Para {
-    private:
-        int a, b, angle, height, per;
-        float area;
-        
-    public:
-        void NumsOfPara()
-        {
-            cout << "enter number a of para" << endl;
-            cin >> a;
-            cout << "enter number b of para" << endl;
-            cin >> b;
-            cout << "enter angle between a and b (if u don`t have enter 0)" << endl;
-            cin >> angle;
-            cout << "enter height of para (if u don`t have enter 0)" << endl;
-            cin >> angle;
-        }
+class Para {
+private:
+    float a;    
+    float b;    
+    float h;  
 
-        void Area()
-        {
-            if (height != 0) {
-                area = a * height;
-                cout << "area is " << area << endl;
-            }
-            else if (angle != 0)
-                {
-                    float sinangle = sin(angle);
-                    area = (a * b * sinangle) / 2;
+public:
+    
+    Para(float a, float b, float h) : a(a), b(b), h(h) {}
 
-                    cout << "area is " << area << endl;
-                }
-            else 
-                cout << "No data" << endl;
-        }
-        
-        void Per() {
-            cout << "per is " << (a + b) * 2 << endl;
-        }
+    float Area()
+    {
+        return a * h;
+    }
+
+    float Per()
+    {
+        return 2 * (a + b);
+    }
+
+    void print() 
+    {
+        cout << "area is "<< Area() << endl;
+        cout << "perimeter is " << Per() << endl;
+    }
+
 };
 
-int main()
+int main() 
 {
-    
+    Para parallelogram(5, 4, 3);
+
+    parallelogram.print();
+
 }
